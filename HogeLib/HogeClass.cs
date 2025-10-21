b4065348dcc255df60e6fc0b4d26f0097821b65e
Kousuke Honda <kousuke.honda@phchd.com>
2025-10-21 16:34:45 +0900

    sample

---
+++ HogeLib/HogeClass.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace HogeLib;
+
+public class HogeClass
+{
+    public int Something(int a) => a;
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using HogeLib;
+
+public class HogeClassTests
+{
+    [Fact]
+    public void Something_Success()
+    {
+        var hoge = new HogeClass();
+        int input = 42;
+        int result = hoge.Something(input);
+        Assert.Equal(input, result);
+    }
+
+    [Fact]
+    public void Something_Fail()
+    {
+        var hoge = new HogeClass();
+        int input = 10;
+        int result = hoge.Something(input);
+        Assert.Equal(0, result);
+    }
+}
+
