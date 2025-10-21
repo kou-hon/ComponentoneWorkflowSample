c4af1477bc06a44f88be6227ec767e8c6c9dbbf6
Kousuke Honda <228415116+kou-hon@users.noreply.github.com>
2025-10-21 16:34:45 +0900

    sample

---
+++ WPFApp/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using HogeLib;
+using System.Windows;
+
+namespace WPFApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        HogeClass hoge = new();
+        this.hoge.Text = $"{hoge.Something(new Random().Next(1, 100))} {DateTimeOffset.Now.ToString("yyMMdd HHmmss zzz")}";
+    }
+}
